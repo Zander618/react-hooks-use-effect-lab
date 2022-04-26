bcf6d99c87951c3a286d35af44f56444e84dc296
Zachary Sirna <zsirna@yahoo.com>
2022-04-26 19:10:37 -0400

    my version

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+    return () => clearTimeout(intervalId);
+  }, [question]);
+    
+  if (timeRemaining === 0) {
+    onAnswered(false);
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -24,7 +33,8 @@ function Question({ question, onAnswered }) {
           </button>
         );
       })}
-      <h5>{timeRemaining} seconds remaining</h5>
+      <h5>{timeRemaining > 0 ? timeRemaining : "No" } seconds remaining</h5>
+      {/* <h5>{onAnswered}</h5> */}
     </>
   );
 }
